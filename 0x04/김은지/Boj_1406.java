304a892ee519228da5f43f0ae30d761edae6b70e
Eundongdong <jk4373@sookmyung.ac.kr>
2024-06-04 10:52:19 +0900

    fix : 240604 boj_1406 Failed

--- 0x04/김은지/Boj_1406.java
+++ 0x04/김은지/Boj_1406.java
@@ -2,51 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Boj_1406 {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String c = br.readLine();
         int M = Integer.parseInt(br.readLine());
         //Linked List 생성
-        LinkedList<Character> contents = new LinkedList<Character>();
+        LinkedList<Character> contents = new LinkedList<>();
         for (int i = 0; i < c.length(); i++){
             contents.add(c.charAt(i));
         }
-        int cursor = c.length() +1;
-        String command;
-        char addContents ;
+        int max_length = c.length();
+        int cursor = max_length;
+        String[] command = new  String[M];
+        char[] addContents = new char[M];
 
+        //커맨드 입력받기
         for (int i = 0; i < M; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            command = st.nextToken();
-            if(Objects.equals(command, "P")){
-                addContents = st.nextToken().charAt(0);
+            command[i] = st.nextToken();
+            if(Objects.equals(command[i], "P")){
+                addContents[i] = st.nextToken().charAt(0);
             }
-            //명령어 수행
-            switch (command){
+
+        }
+
+        //커맨드 실행
+        for (int j = 0 ; j < command.length;j++){
+            switch (command[j]){
                 case "L":
                     if (cursor !=0){
                         cursor -=1;
                     }
                     break;
                 case "D":
-                    if(cursor != c.length()+1){ //
+                    if(cursor != max_length){ //
                         cursor +=1;
                     }
                     break;
                 case "B":
-                    contents.remove(cursor);
+                    if(cursor != 0 && max_length > 0) {
+                        contents.remove(cursor - 1);
+                        cursor -=1;
+                        max_length -=1;
+                    }
                     break;
                 case "P":
-                    contents.add(cursor,addContents);
+                    contents.add(cursor,addContents[j]);
                     cursor +=1;
-                    addContents = '';
-
-
+                    max_length +=1;
+                    break;
             }
+        }
 
 
+
+        for (Character ch : contents){
+            System.out.print(ch);
         }
     }
 }
