742c24fb4b91e898513c5d699a6abeca45ff1ca3
hikhjin <hikhjin@gmail.com>
2024-06-10 22:40:17 +0900

    add : 240610 boj_10773 Solved

---
+++ 0x05/김현진/제로.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int res = 0;
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                stack.pop();
+            } else {
+                stack.push(n);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            res += stack.pop();
+        }
+        System.out.println(res);
+    }
+}
