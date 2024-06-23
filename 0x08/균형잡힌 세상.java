fe9fee625dab8be45926fdc3a0cca9d4f4ecd724
JOLLA99 <yoon6148@daum.net>
2024-06-23 21:39:35 +0900

    add : 240623 boj_4949 Solved

---
+++ 0x08/균형잡힌 세상.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        String answer = "";
+        boolean over = false;
+        
+        while(true) {
+        	
+        	String str = br.readLine();
+        	if(str.equals(".")) { break; }
+        	
+        	// 괄호를 넣을 스택 선언
+            Stack<String> stack = new Stack<>();
+            
+            String[] input = str.split(""); 
+
+            for(String word : input) {
+
+                // 여는 괄호일 경우 스택에 추가
+                if(word.equals("(") || word.equals("[")) {
+                    stack.push(word);
+                }
+
+                if(word.equals(")")) {
+                    if(!stack.isEmpty() && stack.peek().equals("(")) stack.pop();
+                    else stack.push(")");
+                }
+                else if (word.equals("]")) {
+                	if(!stack.isEmpty() && stack.peek().equals("[")) stack.pop();
+                    else stack.push("]");
+                }
+            }
+
+
+            if(stack.empty()) {
+                bw.write("yes\n");
+            } else {
+            	bw.write("no\n");
+            }
+            
+            bw.flush();
+        }
+
+           
+        bw.close();
+    }
+}
