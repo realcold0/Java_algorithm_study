48adafad78ecedd29708d5b5ac48d1530bf0877d
hikhjin <hikhjin@gmail.com>
2024-06-25 17:15:14 +0900

    add : 240625 boj_2504 Solved

---
+++ 0x08/김현진/괄호의_값.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<Character>();
+        String s = br.readLine();
+        s = s.replace("()", "2");
+        s = s.replace("[]", "3"); // 전처리
+
+        int ans = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == ']' || c == ')') {
+                char first = stack.pop();
+                char second = stack.pop();
+                if (second == '[') {
+                    int tmp = Character.getNumericValue(first) * 3;
+                    System.out.println((char) (tmp + '0'));
+                    stack.push((char) (tmp + '0'));
+                } else if (second == '(') {
+                    int tmp = Character.getNumericValue(first) * 2;
+                    System.out.println((char) (tmp + '0'));
+                    stack.push((char) (tmp + '0'));
+                } else { // 숫자일 경우 더하기
+                    int tmp = Character.getNumericValue(first) + Character.getNumericValue(second);
+                    System.out.println(first); // 9
+                    System.out.println(second); // 2
+                    System.out.println(tmp); // 11
+                    System.out.println((char) (tmp + '0'));
+                    stack.push((char) (tmp + '0'));
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        ans = stack.pop();
+
+        if (!stack.empty()) {
+            ans = 0;
+        }
+
+        System.out.println(ans);
+    }
+}
