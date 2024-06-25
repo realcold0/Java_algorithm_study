faff79e61784c6336d5027569049cc874d3dda6a
realcold0 <realcold0@gmail.com>
2024-06-25 16:41:10 +0900

    add : 240625 boj_2504 Failed

---
+++ 0x08/이승환/괄호의_값.java
@@ -0,0 +1,58 @@
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        Stack<Integer> count = new Stack<>();
+        int answer = 0;
+        int tmp = 1;
+        for(int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == '(') {
+                stack.push(input.charAt(i));
+                tmp *= 2;
+            }else if(input.charAt(i) == '[') {
+                stack.push(input.charAt(i));
+                tmp *= 3;
+            }
+            else if(input.charAt(i) == ')') {
+                if(stack.isEmpty() || stack.peek() == '['){
+                    answer = 0;
+                    break;
+                }else{
+                    if(stack.peek() == '('){
+                        answer += tmp;
+                        stack.pop();
+                        tmp /= 2;
+                    }
+                }
+            }
+            else if(input.charAt(i) == ']') {
+                if(stack.isEmpty() || stack.peek() == '('){
+                    answer = 0;
+                    break;
+                }else{
+                    if(stack.peek() == '['){
+                        answer += tmp;
+                        stack.pop();
+                        tmp /= 3;
+                    }
+                }
+            }
+        }
+
+        if(!stack.isEmpty()){
+            bw.write(String.valueOf(0));
+        }else{
+            bw.write(String.valueOf(answer));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
