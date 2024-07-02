43f60b9b32df7b5610e2c7074ac2a3f1215b4748
Eundongdong <jk4373@sookmyung.ac.kr>
2024-07-02 16:16:29 +0900

    fix : 240702 boj_2573 Solved

--- 0x09/김은지/빙산.java
+++ 0x09/김은지/빙산.java
@@ -6,30 +6,37 @@ import java.util.StringTokenizer;
 public class 빙산 {
     static int n;
     static int m;
-    static int cnt;
     static int year;
     public static int[][]dirs = {{1,0},{-1,0},{0,1},{0,-1}};
 
     //0 파도일 때 4방향 -1
-    public static void bfsWave(int[][]map,int startX, int startY) {
-        for (int i = 0; i < 4; i++) {
-            int nx = dirs[i][0] + startX;
-            int ny = dirs[i][1] + startY;
-            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                if (map[nx][ny] > 0) {
-                    map[nx][ny] -= 1;
+    public static void bfsWave(int[][]map) {
+        int[][]temp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(map[i][j]>0){
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nx = dirs[dir][0] + i;
+                        int ny = dirs[dir][1] + j;
+                        if (0 <= nx && nx < n && 0 <= ny && ny < m){
+                            if(map[nx][ny]==0){
+                                temp[i][j]--;
+                            }
+                        }
+                    }
+                    temp[i][j] = Math.max(0,map[i][j] +temp[i][j]);
                 }
             }
         }
-        return;
+
+        for (int i = 0; i < n; i++) {
+        System.arraycopy(temp[i], 0, map[i], 0, m);
+        }
     }
 
-    public static void bfsCount(int[][]map, int startX, int startY,boolean[][]visited, int cnt){
+    public static void bfsCount(int[][]map, int startX, int startY,boolean[][]visited){
         Queue<int[]> q=  new LinkedList<>();
-
-
         //일단 면적 1 증가
-        cnt++;
         q.add(new int[]{startX,startY});
         visited[startX][startY] = true;
         while(!q.isEmpty()){
@@ -66,35 +73,32 @@ public class 빙산 {
             }
         }
 
-        boolean flag = false;
-        while(cnt<2){
-            cnt = 0;
-            flag =false;
+//        boolean flag = false;
+        while(true){
+            int cnt = 0;
             year+=1;
              //파도 역할
-            for(int i = 0; i<n;i++){
-                for(int j = 0; j<m;j++){
-                    if(map[i][j] ==0)
-                        bfsWave(map,i,j);
-                }
-            }
+            bfsWave(map);
             //빙하 개수 새기
             boolean[][] visited = new boolean[n][m];
             for(int i = 0; i<n;i++){
                 for(int j = 0; j<m;j++){
-                    if(map[i][j] !=0){
-                        flag = true;
-                        bfsCount(map,i,j,visited,cnt);
+                    if(map[i][j] !=0 && !visited[i][j]){
+                        cnt++;
+                        bfsCount(map,i,j,visited);
                     }
                 }
             }
+            if(cnt ==0){
+                System.out.println(0);
+                break;
+            }else if(cnt>=2){
+                System.out.println(year);
+                break;
+            }
         }
 
-        if(!flag){
-            System.out.println(year);
-        }else{
-            System.out.println(cnt);
-        }
+
 
 
 
