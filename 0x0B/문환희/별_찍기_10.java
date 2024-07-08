2aa35133f4ecb46325f8bd366512f9614aeaaa95
HwanheeMoon <yuiop0218@naver.com>
2024-07-08 13:44:59 +0900

    fix: 240708 boj_2447 Solved

--- 0x0B/문환희/별_찍기_10.java
+++ 0x0B/문환희/별_찍기_10.java
@@ -11,29 +11,28 @@ public class 별_찍기_10 {
         }
         else {
             int size = n / 3;
+            map[i][j] = ' ';
             for (int p = 0; p < 3; p++) {
                 for (int q = 0; q < 3; q++) {
                     if(p == 1 && q == 1) {
-                        map[p][q] = ' ';
+                        continue;
                     }
-                    else {
-                        print(size, i + p * size, j + q * size);
-                    }
+                    print(size, i + p * size, j + q * size);
                 }
             }
-
         }
     }
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = scanner.nextInt();
         map = new char[n][n];
-//        for(int i = 0; i < n; i++) {
-//            for(int j = 0; j < n; j++) {
-//                map[i][j] = ' ';
-//            }
-//        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                map[i][j] = ' ';
+            }
+        }
         print(n, 0, 0);
 
         for(int i = 0; i < n; i++) {
