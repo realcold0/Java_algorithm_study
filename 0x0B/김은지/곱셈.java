b2441bead78d952ac03627b427454fbc98d056ff
Eundongdong <jk4373@sookmyung.ac.kr>
2024-07-09 15:09:03 +0900

    fix : 240709 boj_1629 Solved

--- 0x0B/김은지/곱셈.java
+++ 0x0B/김은지/곱셈.java
@@ -2,23 +2,24 @@
 import java.util.*;
 import java.io.*;
 
-public class 곱셈 {
+public class 곱셈{
 
     public static long recur(long a, long b, long c){
+        if(b == 0){
+            return 1; // b가 0인 경우, 결과는 1이어야 함
+        }
         if(b == 1){
-            return a % b;
+            return a % c;
         }else{
             long val = recur(a, b/2, c);
-            if( b%2==0){
-                return (val*val) %c;
+            val = (val * val) % c; // 중복된 계산을 방지하기 위해 한 번 계산
+            if(b % 2 == 0){
+                return val;
             }else{
-                return (val*val*a) %c;
+                return (val * a) % c;
             }
-
         }
-
     }
-
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,11 +29,11 @@ public class 곱셈 {
         long b = Long.parseLong(st.nextToken());
         long c = Long.parseLong(st.nextToken());
 
-        bw.write(String.valueOf(recur(a,b,c)));
+        long result = recur(a, b, c);
+        System.out.println(result);
+        bw.write(String.valueOf(result));
         bw.flush();
         bw.close();
         br.close();
     }
-
 }
-
