180d3eb1aeb224c2ea534030e5c881223cc9a086
hikhjin <hikhjin@gmail.com>
2024-08-05 19:46:45 +0900

    add: 240805 boj_7795 Solved

---
+++ 0x0E/김현진/먹을_것인가_먹힐_것인가_7795.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            Integer arrA[] = new Integer[N];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arrA[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Integer arrB[] = new Integer[M];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arrB[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arrA, Collections.reverseOrder());
+            Arrays.sort(arrB);
+
+            int cnt = 0;
+
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < M; k++) {
+                    if (arrA[j] > arrB[k]) {
+                        cnt++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            bw.write(cnt + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
