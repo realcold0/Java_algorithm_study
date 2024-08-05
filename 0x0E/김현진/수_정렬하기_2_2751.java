d3eaf18e081aa20f8f9eefa176ad51a89a1de6d0
hikhjin <hikhjin@gmail.com>
2024-08-05 20:13:18 +0900

    add: 240805 boj_2751 Solved

---
+++ 0x0E/김현진/수_정렬하기_2_2751.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        int [] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+}
