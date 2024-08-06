ed21387c3589c072667ab750b2e673a416f03494
hikhjin <hikhjin@gmail.com>
2024-08-06 16:58:47 +0900

    Modify: 240805 boj_7795 Solved

--- 0x0E/김현진/먹을_것인가_먹힐_것인가_7795.java
+++ 0x0E/김현진/먹을_것인가_먹힐_것인가_7795.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.StringTokenizer;
@@ -6,7 +8,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
 
@@ -42,9 +43,7 @@ public class Main {
                     }
                 }
             }
-            bw.write(cnt + "\n");
+            System.out.println(cnt);
         }
-        bw.flush();
-        bw.close();
     }
 }
