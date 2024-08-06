25f53db0f3a677c64a74b83aaa1891cd1bc5fa43
HwanheeMoon <yuiop0218@naver.com>
2024-08-06 14:40:46 +0900

    add : 240806 boj_2751 Solved

---
+++ 0x0E/문환희/수_정렬하기2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 수_정렬하기2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.parallelSort(arr);
+
+        for(int i : arr) {
+            bw.write(i + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
