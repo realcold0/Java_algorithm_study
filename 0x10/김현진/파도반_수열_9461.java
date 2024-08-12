36c09e93ef3ba7f48541dc10753a0d5b65396862
hikhjin <hikhjin@gmail.com>
2024-08-12 13:41:08 +0900

    add: 240812 boj_9461 Solved

---
+++ 0x10/김현진/파도반_수열_9461.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
+        dp[7] = 4;
+        dp[8] = 5;
+        dp[9] = 7;
+        dp[10] = 9;
+
+        for (int j = 11; j <= 100; j++) {
+            dp[j] = dp[j-1] + dp[j-5];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n]).append("\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
