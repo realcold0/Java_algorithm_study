8f5e739306deecf377577a746462121475e80810
realcold0 <realcold0@gmail.com>
2024-08-12 14:46:21 +0900

    add : 240812 boj_1463 Solved

---
+++ 0x10/이승환/1로만들기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[1] = 0;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if(i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i/2] + 1);
+            }
+            if(i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            }
+        }
+        bw.write(dp[n] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
