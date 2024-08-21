b8e7481a62bb3865c0746acd88b84cd0117f9bdd
Eundongdong <jk4373@sookmyung.ac.kr>
2024-08-21 15:06:42 +0900

    fix: 240821 boj_1744 Solved

--- 0x11/김은지/수묶기.java
+++ 0x11/김은지/수묶기.java
@@ -1,49 +1,56 @@
 package boj.greedy.groupnumber;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Integer[] arr = new Integer[n];
-        boolean[] visited = new boolean[n];
-        int minusCnt = 0;
+        List<Integer> positives = new ArrayList<>();
+        List<Integer> negatives = new ArrayList<>();
+        int ones = 0;
+        int zeros = 0;
+
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if (arr[i] <0) minusCnt++;
-        }
-        Arrays.sort(arr,Collections.reverseOrder());
-        int sum = Arrays.stream(arr).mapToInt(Integer::intValue).sum();
-        int multiSum = Integer.MIN_VALUE;
-        int i = 0;
-        while( i < n-1){
-            if(arr[i] * arr[i+1] >0 || (arr[i] ==0 && arr[i+1] < 0 && (minusCnt %2 !=0))){
-                if(multiSum == Integer.MIN_VALUE){
-                    multiSum =0;
-                }
-                multiSum += arr[i]*arr[i+1];
-                visited[i] = true;
-                visited[i+1] = true;
-                i +=2;
-            }else{
-                if(multiSum == Integer.MIN_VALUE){
-                    multiSum =0;
-                }
-                multiSum += arr[i];
-                visited[i] = true;
-                i++;
+            int num = Integer.parseInt(br.readLine());
+            if (num > 1) {
+                positives.add(num);
+            } else if (num == 1) {
+                ones++;
+            } else if (num == 0) {
+                zeros++;
+            } else {
+                negatives.add(num);
             }
-        }
-        if(!visited[n - 1]){
-            if(multiSum == Integer.MIN_VALUE){
-                multiSum =0;
-            }
-            multiSum += arr[n-1];
         }
 
-        System.out.println(Math.max(sum, multiSum));
+        positives.sort(Collections.reverseOrder());
+        Collections.sort(negatives);
+
+        long result = 0;
+
+        // 큰 양수끼리 묶어서 곱한다.
+        for (int i = 0; i < positives.size(); i += 2) {
+            if (i + 1 < positives.size()) {
+                result += (long) positives.get(i) * positives.get(i + 1);
+            } else {
+                result += positives.get(i);
+            }
+        }
+
+        // 작은 음수끼리 묶어서 곱한다.
+        for (int i = 0; i < negatives.size(); i += 2) {
+            if (i + 1 < negatives.size()) {
+                result += (long) negatives.get(i) * negatives.get(i + 1);
+            } else if (zeros == 0) {
+                result += negatives.get(i);
+            }
+        }
+
+        // 1은 곱하기보다는 더하는 것이 유리하다.
+        result += ones;
+
+        System.out.println(result);
     }
 }
