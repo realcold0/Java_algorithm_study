21844f23be0064483debbe4ab282f3ddad846ca7
hikhjin <hikhjin@gmail.com>
2024-09-25 21:39:31 +0900

    add: 240925 boj_15903 Solved

---
+++ all_part_3/김현진/카드_합체_놀이_15903.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 카드 개수
+        int m = Integer.parseInt(st.nextToken()); // 합체 횟수
+        st = new StringTokenizer(br.readLine());
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            pq.offer(Long.parseLong(st.nextToken()));
+        }
+
+        for (int i = 0; i < m; i++) {
+            long tmp = pq.poll() + pq.poll();
+            pq.offer(tmp);
+            pq.offer(tmp);
+        }
+
+        long ans = 0;
+        while (!pq.isEmpty()) {
+            ans += pq.poll();
+        }
+
+        System.out.println(ans);
+
+    }
+}
